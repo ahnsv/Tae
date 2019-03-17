0b67f4e1841b085a3ba0e63eec86abfec732db22
ahnopologetic <ahnsv@bc.edu>
2019-03-17 10:36:54 +0900

    feat: splitting svg files

---
+++ components/svgs/index.js
@@ -0,0 +1,9 @@
+import FirstSvgs from './FirstTierSvgsContainer'
+import SecondSvgs from './SecondTierSvgsContainer'
+import ThirdSvgs from './ThirdTierSvgsContainer'
+
+export default {
+    FirstSvgs,
+    SecondSvgs,
+    ThirdSvgs
+}
